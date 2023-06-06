c6b749b1a6c70410b1d9fa6347a26677e274cf46
Kaleb <73762967+Klobbb@users.noreply.github.com>
2023-06-05 22:47:05 -0700

    Add files via upload

---
+++ PortRange.java
@@ -0,0 +1,18 @@
+package portScanner;
+
+
+		// TODO Auto-generated method stub
+		public class PortRange {
+		    public int min;
+		    public int max;
+
+		    public final boolean isPortBusy;
+
+		    public PortRange(int minPort, boolean isPortBusy) {
+		        this.min = minPort;
+		        this.max = minPort;
+		        this.isPortBusy = isPortBusy;
+		    }
+		}
+
+
